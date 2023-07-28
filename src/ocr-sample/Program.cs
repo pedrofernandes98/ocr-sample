2458a22e79d7aa870a8f71d0a19dde4b353d0f4b
Pedro Fernandes <ppedrohfs@gmail.com>
2023-07-27 21:12:08 -0300

    first

---
+++ src/ocr-sample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tesseract;
+
+namespace ocr_sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando o programa");
+
+            var path = "C:\\dev\\playground\\ocr-sample\\teste.png";
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"C:\dev\playground\ocr-sample\src\tessdata", "por", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(path))
+                    {
+                        using(var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+
+                            Console.WriteLine($"Taxa de precisão: {page.GetMeanConfidence()}");
+                            Console.WriteLine(text);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Fim do programa");
+                Console.ReadLine();
+            }
+        }
+    }
+}
